4c91039d5f8d5b9b45f4c96da7d9d983ad0c142c
steeeee0223 <mr.steven7@gmail.com>
2023-12-15 08:09:39 +0000

    ✨ Setup project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rust Data Structures & Algorithms
+
+### Important Commands
+
+1. LeetCode file generator: `cargo r --bin leet <index> <function_name>`
---
+++ src/bin/leet.rs
@@ -0,0 +1,77 @@
+extern crate handlebars;
+
+use handlebars::{to_json, Handlebars};
+use serde_json::value::{Map, Value as Json};
+use std::{env, error::Error, fs::File, process};
+
+pub struct Config {
+    pub index: String,
+    pub func: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("Not enough arguments!");
+        }
+
+        let index = args[1].clone();
+        let func = args[2].clone();
+
+        Ok(Config { index, func })
+    }
+}
+
+pub fn make_data(config: &Config) -> Map<String, Json> {
+    let mut data = Map::new();
+    data.insert("func".to_string(), to_json(&config.func));
+    data.insert("num".to_string(), to_json(&config.index));
+    data
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut handlebars = Handlebars::new();
+    let data = make_data(&config);
+
+    handlebars
+        .register_template_file("template", "./templates/leetcode/template.hbs")
+        .unwrap();
+
+    let path = format!("src/leetcode/leetcode_{}.rs", config.index);
+    let mut output_file = File::create(&path)?;
+    handlebars.render_to_write("template", &data, &mut output_file)?;
+
+    let lib_file = File::options()
+        .append(true)
+        .open("./src/leetcode.rs")
+        .unwrap();
+    handlebars
+        .register_template_string("import", "mod leetcode_{{num}};\n")
+        .unwrap();
+    handlebars
+        .render_to_write("import", &data, &lib_file)
+        .unwrap();
+    handlebars
+        .register_template_string("export", "pub use leetcode_{{num}}::*;\n")
+        .unwrap();
+    handlebars
+        .render_to_write("export", &data, &lib_file)
+        .unwrap();
+
+    println!("✅ Generated file: {}", &path);
+    Ok(())
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("🚨 Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    println!("🎯 Leetcode {}: {}", config.index, config.func);
+
+    if let Err(e) = run(config) {
+        println!("🚨 Application error: {e}");
+        process::exit(1);
+    }
+}
